3f49af7479e20cc9393625a451810daf7473abc0
liuye <liuye@diligrp.com>
2018-11-08 18:31:02 +0800

    feat;electron-vue练习项目

---
+++ src/renderer/components/LoginPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wrapper">
+    <main>
+      <el-input v-model="user" placeholder="用户名"></el-input>
+      <el-input v-model="password" placeholder="密码"></el-input>
+    </main>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'login-page',
+    components: { SystemInformation },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+  el-input {color: aquamarine}
+  .wrapper {
+    background: url('~@/assets/login-bg.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 462px;
+    width: 1186px;
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  main > div { flex-basis: 50%; }
+
+  .user {
+    display: flex;
+    left: 1000px;
+    top: 100px;
+    width: 80px;
+    height: 20px;
+  }
+
+  .password {
+    display: flex;
+    left: 1000px;
+    top: 100px;
+    width: 80px;
+    height: 20px;
+  }
+</style>
